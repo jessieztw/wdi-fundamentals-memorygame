623ecd74bb07de38156f3a9b6af1346a809d8102
Jessie Wong <jessieztwong@gmail.com>
2020-10-21 22:01:35 +1100

    created game logic

---
+++ memory-game/js/main.js
@@ -0,0 +1,58 @@
+console.log("Up and running!");
+
+
+const cards = [
+{
+	rank: "queen",
+	suit: "hearts",
+	cardImage: "images/queen-of-hearts.png"
+
+},
+{
+	rank: "queen",
+	suit: "diamonds",
+	cardImage: "images/queen-of-diamonds.png"
+},
+{
+	rank: "king",
+	suit: "hearts",
+	cardImage: "images/king-of-hearts.png"
+},
+{
+	rank: "king",
+	suit: "diamonds",
+	cardImage: "images/king-of-diamonds.png"
+}
+];
+
+const cardsInPlay = [];
+
+function checkForMatch() {
+	if(cardsInPlay[0] === cardsInPlay [1]) 
+	{ alert("You found a match!");
+	} else { 
+	  alert("Sorry, try again");
+	}
+
+}
+
+function flipCard(cardId) { 
+	cardsInPlay.push(cards[cardId].rank);
+	console.log("User flipped " + cards[cardId].rank);
+	console.log(cards[cardId].cardImage);
+	console.log(cards[cardId].suit);
+	checkForMatch();
+
+};
+
+
+
+flipCard(0);
+
+flipCard(0);
+
+
+
+
+
+	
