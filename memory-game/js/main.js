cbe7ed1df0de63283fce83a2de6e337ba66e8284
Jessie Wong <jessieztwong@gmail.com>
2020-10-26 20:55:45 +1100

    created game logic

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -27,16 +27,29 @@ const cards = [
 
 const cardsInPlay = [];
 
+function createBoard() {
+	for(let i = 0; i < cards.length; i++) {
+		let cardElement = document.createElement("img");
+		cardElement.setAttribute("src","images/back.png");
+		cardElement.setAttribute("data-id",i);
+		cardElement.addEventListener("click",flipCard);
+		document.getElementById("game-board").appendChild(cardElement);
+	}
+}
+
 function checkForMatch() {
 	if(cardsInPlay[0] === cardsInPlay [1]) 
 	{ alert("You found a match!");
 	} else { 
 	  alert("Sorry, try again");
-	}
+	};
 
-}
+};
 
-function flipCard(cardId) { 
+
+function flipCard() { 
+	var cardId = this.getAttribute("data-id");
+	this.setAttribute("src",cards[cardId].cardImage);
 	cardsInPlay.push(cards[cardId].rank);
 	console.log("User flipped " + cards[cardId].rank);
 	console.log(cards[cardId].cardImage);
@@ -47,9 +60,11 @@ function flipCard(cardId) {
 
 
 
-flipCard(0);
 
-flipCard(0);
+
+
+
+createBoard();
 
 
 
